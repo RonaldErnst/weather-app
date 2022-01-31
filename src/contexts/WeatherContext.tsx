1c44e682c420b85eef6b357ad67acaea575b621f
RonaldErnst <eronald@web.de>
2022-01-31 22:23:04 +0100

    Folder structure init

--- src/contexts/WeatherContext.tsx
+++ src/contexts/WeatherContext.tsx
@@ -1,99 +1,108 @@
-import React, { useState, useContext, useEffect, FC } from 'react';
-import { openweathermap } from '../apiKeys';
-
+import React, { useState, useContext, useEffect, FC } from "react";
+import { openweathermap } from "../apiKeys";
 
 const resetIntevalTime = 30 * 1000; // Fetch Weather Data every 30 secs
 
 const WeatherContext = React.createContext<any>(null);
 
 export const unitTypes = {
-    METRIC: "metric",
-    IMPERIAL: "imperial"
+	METRIC: "metric",
+	IMPERIAL: "imperial",
 };
 
 export function useWeather() {
-    return useContext(WeatherContext);
+	return useContext(WeatherContext);
 }
 
-type LocationType = {lat: number, lon: number};
+function getLang() {
+	if (navigator.languages !== undefined)
+        return navigator.languages[0];
+	return navigator.language;
+}
 
-function getCurrentPosition() : Promise<GeolocationPosition | null> {
-    if (navigator.geolocation) {
-      return new Promise(
-        (resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject)
-      );
-    } else {
-      return new Promise(
-        resolve => resolve(null)
-      );
-    }
-  }
+type LocationType = { lat: number; lon: number; locale: string };
+const defaultLocation: LocationType = {
+	lat: 28.67,
+	lon: 77.22,
+	locale: getLang(),
+};
+
+function getCurrentPosition(): Promise<GeolocationPosition | null> {
+	if (navigator.geolocation) {
+		return new Promise((resolve, reject) =>
+			navigator.geolocation.getCurrentPosition(resolve, reject)
+		);
+	} else {
+		return new Promise((resolve) => resolve(null));
+	}
+}
 
 async function getDefaultLocation() {
-    let location: LocationType = {
-        lat: 28.67,
-        lon: 77.22
-    }; // Default Location London
+	let location = defaultLocation; // Default Location London
 
-    if (navigator.geolocation) {
-        const response = await getCurrentPosition();
-        if(response !== null)
-            location = {
-                lat: response.coords.latitude as number,
-                lon: response.coords.longitude as number
-            };
-    }
+	if (navigator.geolocation) {
+		const response = await getCurrentPosition();
+		if (response !== null)
+			location = {
+				lat: response.coords.latitude as number,
+				lon: response.coords.longitude as number,
+				locale: getLang(),
+			};
+	}
 
-    return location;
+	return location;
 }
 
-export const WeatherProvider: FC = ({children}) => {
-    const [weatherData, setWeatherData] = useState({});
-    const [location, setLocation] = useState({lat: 28.67, lon: 77.22});
-    const [tempUnit, setTempUnit] = useState(unitTypes.METRIC);
+export const WeatherProvider: FC = ({ children }) => {
+    const isMetric = !["en-US", "my"].includes(getLang());
 
-    useEffect(() => {
-        // Location changed
-        async function getWeather({lat, lon} : LocationType) {
-            const api_call = await fetch(
-                `${openweathermap.url}onecall?lat=${lat}&lon=${lon}&appid=${openweathermap.key}`
-            );
-            const data = await api_call.json();
-            setWeatherData(data);
-        };
+	const [weatherData, setWeatherData] = useState({});
+	const [location, setLocation] = useState(defaultLocation);
+	const [tempUnit, setTempUnit] = useState(isMetric? unitTypes.METRIC : unitTypes.IMPERIAL);
 
-        getWeather(location);
+	useEffect(() => {
+		// Location changed
+		async function getWeather({ lat, lon }: LocationType) {
+			const api_call = await fetch(
+				`${openweathermap.url}onecall?lat=${lat}&lon=${lon}&lang=${location.locale}&exclude=minutely,hourly,alerts&appid=${openweathermap.key}`
+			);
+			const data = await api_call.json();
+			setWeatherData(data);
+		}
 
-        // If location changed, also change update timer
-        const refreshInterval = setInterval(async () => {
-            getWeather(location);
-        }, resetIntevalTime);
+		getWeather(location);
 
-        return () => {
-            clearInterval(refreshInterval);
-        }
-    }, [location]);
+		// If location changed, also change update timer
+		const refreshInterval = setInterval(async () => {
+			getWeather(location);
+		}, resetIntevalTime);
 
-    useEffect(() => {
-        // Get inital Location
-        async function getInitialLocation() {
-            const location = await getDefaultLocation() as LocationType;
-            setLocation(location);
-        };
+		return () => {
+			clearInterval(refreshInterval);
+		};
+	}, [location]);
 
-        getInitialLocation();
-    }, []);
+	useEffect(() => {
+		// Get inital Location
+		async function getInitialLocation() {
+			const location = (await getDefaultLocation()) as LocationType;
+			setLocation(location);
+		}
 
+		getInitialLocation();
+	}, []);
 
-    return (
-        <WeatherContext.Provider value={{
-            location,
-            weatherData,
-            tempUnit,
-            setLocation,
-            setTempUnit
-        }}>
-            {children}
-        </WeatherContext.Provider>
-    )
-}
+	return (
+		<WeatherContext.Provider
+			value={{
+				weatherData,
+				location,
+				tempUnit,
+				setLocation,
+				setTempUnit,
+			}}
+		>
+			{children}
+		</WeatherContext.Provider>
+	);
+};
